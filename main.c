197247773395828e78b371e0f3a04233d6f2a2a5
ale-polimi <10742196@polimi.it>
2023-07-14 18:59:08 +0200

    Added functions to demolish a station, add/remove a vehicle from a station

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include <string.h>
+
+#define FALSE 0
+#define TRUE (!FALSE)
 
 /*
  * Comandi:
@@ -24,10 +26,12 @@ typedef struct station {
 
 typedef t_station* ptr_station;
 
+typedef int bool;
+
 ptr_station addStation(ptr_station, uint32_t, uint32_t*);
-ptr_station removeStation(uint32_t);
-void addVehicle(uint32_t, uint32_t);
-void removeVehicle(uint32_t, uint32_t);
+ptr_station removeStation(ptr_station, uint32_t);
+void addVehicle(ptr_station, uint32_t, uint32_t);
+void removeVehicle(ptr_station, uint32_t, uint32_t);
 void printStations(ptr_station);
 
 
@@ -39,7 +43,7 @@ int main() {
     uint32_t veicoli1[512] = { 0 };
     uint32_t veicoli2[512] = { 0 };
     uint32_t veicoli3[512] = { 0 };
-    int i;
+    uint32_t veicoli4[512] = { 0 };
 
     veicoli1[0] = 10;
     veicoli1[1] = 3;
@@ -53,14 +57,21 @@ int main() {
     veicoli2[4] = 23;
 
     veicoli3[0] = 12;
+    veicoli4[0] = 1234;
 
     autostrada = addStation(autostrada, 10, veicoli1);
-    printStations(autostrada);
-
     autostrada = addStation(autostrada, 2, veicoli2);
-    printStations(autostrada);
+    autostrada = addStation(autostrada, 11, veicoli3);
+    //autostrada = addStation(autostrada, 11, veicoli4);
+    //autostrada = addStation(autostrada, 20, veicoli4);
 
-    autostrada = addStation(autostrada, 5, veicoli3);
+    //autostrada = removeStation(autostrada, 12);
+    addVehicle(autostrada, 10, 10);
+    //printStations(autostrada);
+    addVehicle(autostrada, 3, 123);
+    addVehicle(autostrada, 20, 2);
+    printStations(autostrada);
+    removeVehicle(autostrada, 10, 12);
     printStations(autostrada);
 
     return 0;
@@ -84,16 +95,17 @@ void printStations(ptr_station ptStations){
 }
 
 /**
- * Aggiunta in ordine crescente di una stazione all'autostrada.
+ * Aggiunta in ordine crescente di una stazione all'autostrada. Se è già presente una stazione di servizio alla distanza specificata, non la si aggiunge.
  * @param ptStations è il puntatore all'inizio della lista.
  * @param distance è la distanza della nuova stazione dall'inizio dell'autostrada.
  * @param vehicles è il puntatore all'array contenente i veicoli della nuova stazione.
- * @return il puntatore al'inizio della lista aggiornata.
+ * @return il puntatore all'inizio della lista aggiornata.
  */
 ptr_station addStation(ptr_station ptStations, uint32_t distance, uint32_t* vehicles){
     ptr_station ptTempToAdd;
     ptr_station ptTemp;
     ptr_station ptTempPrevious;
+    bool alreadyPresent = FALSE;
     int i;
 
     ptTempToAdd = malloc(sizeof (t_station));
@@ -118,22 +130,164 @@ ptr_station addStation(ptr_station ptStations, uint32_t distance, uint32_t* vehi
                 ptTemp = ptTemp->next;
             }
 
-            if(ptTempPrevious == NULL){
-                ptStations->previous = ptTempToAdd;
-                ptTempToAdd->next = ptStations;
-                ptStations = ptTempToAdd;
-            } else {
-                /* Collego il nodo precedente al nuovo nodo */
-                ptTempPrevious->next = ptTempToAdd;
-                /* Collego il nuovo nodo al nodo precedente */
-                ptTempToAdd->previous = ptTempPrevious;
-                /* Collego il nuovo nodo al successivo */
-                ptTempToAdd->next = ptTemp;
-                /* Collego il nodo successivo al nuovo nodo */
-                ptTemp->previous = ptTempToAdd;
+            if(ptTemp != NULL && ptTemp->distance == ptTempToAdd->distance){
+                alreadyPresent = TRUE;
             }
+
+            if(!alreadyPresent) {
+                if (ptTempPrevious == NULL) {
+                    /* Aggiungo in testa */
+                    ptStations->previous = ptTempToAdd;
+                    ptTempToAdd->next = ptStations;
+                    ptStations = ptTempToAdd;
+                } else if (ptTemp == NULL) {
+                    /* Aggiungo in coda */
+                    /* Collego il nodo precedente al nuovo nodo */
+                    ptTempPrevious->next = ptTempToAdd;
+                    /* Collego il nuovo nodo al nodo precedente */
+                    ptTempToAdd->previous = ptTempPrevious;
+                    /* Collego il nuovo nodo al successivo */
+                    ptTempToAdd->next = ptTemp;
+                } else {
+                    /* Collego il nodo precedente al nuovo nodo */
+                    ptTempPrevious->next = ptTempToAdd;
+                    /* Collego il nuovo nodo al nodo precedente */
+                    ptTempToAdd->previous = ptTempPrevious;
+                    /* Collego il nuovo nodo al successivo */
+                    ptTempToAdd->next = ptTemp;
+                    /* Collego il nodo successivo al nuovo nodo */
+                    ptTemp->previous = ptTempToAdd;
+                }
+            }
+        }
+
+        if(!alreadyPresent){
+            printf("aggiunta stazione\n");
+        } else {
+            printf("non aggiunta stazione\n");
+            free(ptTempToAdd);
         }
     }
 
     return(ptStations);
+}
+
+/**
+ * Rimozione di una stazione di servizio. Se non è presente una stazione alla distanza specificata, non la si demolisce.
+ * @param ptStations è il puntatore all'inizio dell'autostrada.
+ * @param distance è la distanza alla quale è presente la stazione da demolire.
+ * @return il puntatore all'inizio della lista aggiornata.
+ */
+ptr_station removeStation(ptr_station ptStations, uint32_t distance){
+    ptr_station ptTemp;
+    ptr_station ptTempPrevious;
+    bool found = TRUE;
+
+    ptTemp = ptStations;
+    ptTempPrevious = NULL;
+    while(ptTemp != NULL && ptTemp->distance != distance){
+        ptTempPrevious = ptTemp;
+        ptTemp = ptTemp->next;
+    }
+
+    if(ptTemp == NULL){
+        found = FALSE;
+    } else {
+        if(ptTempPrevious == NULL){
+            ptTemp->next->previous = ptTempPrevious;
+            ptStations = ptTemp->next;
+            free(ptTemp);
+        } else if(ptTemp->next == NULL){
+            ptTempPrevious->next = NULL;
+            free(ptTemp);
+        } else {
+            ptTempPrevious->next = ptTemp->next;
+            ptTemp->next->previous = ptTempPrevious;
+            free(ptTemp);
+        }
+    }
+
+    if(!found){
+        printf("non demolita\n");
+    } else {
+        printf("demolita\n");
+    }
+
+    return(ptStations);
+}
+
+/**
+ * Aggiunta di un veicolo nel parco macchine della stazione di servizio. Se la stazione non esiste, non si aggiunge il veicolo.
+ * @param ptStations è il puntatore all'inizio dell'autostrada.
+ * @param distance è la distanza della stazione alla quale si deve aggiungere il veicolo.
+ * @param vehicle è il veicolo da aggiungere.
+ */
+void addVehicle(ptr_station ptStations, uint32_t distance, uint32_t vehicle){
+    ptr_station ptTemp;
+    bool found = TRUE;
+    bool addedVehicle;
+    int i;
+
+    ptTemp = ptStations;
+    while(ptTemp != NULL && ptTemp->distance != distance){
+        ptTemp = ptTemp->next;
+    }
+
+    if(ptTemp == NULL){
+        found = FALSE;
+    } else {
+        i = 0;
+        addedVehicle = FALSE;
+        while(!addedVehicle && i < 512){
+            if(ptTemp->vehiclesInStation[i] == 0){
+                ptTemp->vehiclesInStation[i] = vehicle;
+                addedVehicle = TRUE;
+            }
+            i++;
+        }
+    }
+
+    if(!found){
+        printf("non aggiunta auto\n");
+    } else {
+        printf("aggiunta auto\n");
+    }
+}
+
+/**
+ * Rimozione di un veicolo dal parco macchine della stazione di servizio. Se la stazione non esiste o se nella stazione il veicolo non esiste non lo si rottama.
+ * @param ptStations è il puntatore all'inizio dell'autostrada.
+ * @param distance è la distanza della stazione dalla quale si deve rimuovere il veicolo.
+ * @param vehicleToRemove è il veicolo da rimuovere.
+ */
+void removeVehicle(ptr_station ptStations, uint32_t distance, uint32_t vehicleToRemove){
+    ptr_station ptTemp;
+    bool found = TRUE;
+    bool removedVehicle;
+    int i;
+
+    ptTemp = ptStations;
+    while(ptTemp != NULL && ptTemp->distance != distance){
+        ptTemp = ptTemp->next;
+    }
+
+    if(ptTemp == NULL){
+        found = FALSE;
+    } else {
+        i = 0;
+        removedVehicle = FALSE;
+        while(!removedVehicle && i < 512){
+            if(ptTemp->vehiclesInStation[i] == vehicleToRemove){
+                ptTemp->vehiclesInStation[i] = 0;
+                removedVehicle = TRUE;
+            }
+            i++;
+        }
+    }
+
+    if(!found || !removedVehicle){
+        printf("non rottamata\n");
+    } else {
+        printf("rottamata\n");
+    }
 }
